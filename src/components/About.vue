ec80109db976c423bace957441c4f8f4683f91f2
Nikolaus.Stift <nstift.work@gmail.com>
2023-10-19 17:43:57 +0200

    Inhalt About Page

--- src/components/About.vue
+++ src/components/About.vue
@@ -13,12 +13,41 @@ export default defineComponent({
       <div class="col-md-6 offset-md-3">
         <img src="@/assets/Aboutpic.jpg" class="img-fluid mb-4" alt="About Us Image">
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil laborum eligendi laudantium earum id assumenda molestias esse consectetur 
-          fuga quos! Vitae nemo rerum magni iste magnam neque explicabo nulla, similique modi culpa, quia dignissimos itaque tenetur distinctio. 
-          Facilis deleniti error consectetur repudiandae culpa, sunt voluptate ad illo ex vitae, temporibus reprehenderit possimus ut autem minima.
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita facere porro voluptate maxime, atque impedit asperiores ex optio aliquam sed 
-          similique eaque fuga molestiae et distinctio perspiciatis nobis sequi repellat 
-          natus consequatur mollitia quos pariatur ad? Ipsa ipsum commodi quasi, eveniet quos nam voluptatibus ducimus sunt pariatur? Quasi, sequi quia?
+          Herzlich willkommen bei Boutique-Hotel Technikum Wien!
+          Lassen Sie uns Ihnen mehr über unsere Leidenschaft für Gastfreundschaft, Technologie und Wiener Kultur erzählen.
+          Unser Boutique-Hotel ist das Ergebnis einer Vision, die von der Idee getragen wird, eine einzigartige und innovative Erfahrung für Reisende zu schaffen.
+          Wir sind fest davon überzeugt, dass Gastfreundschaft mehr ist als nur ein Geschäft – es ist eine Kunst. Diese Kunst üben wir mit Hingabe und Begeisterung aus.
+        </p>
+        <h3>Unsere Mission</h3>
+        <p>
+          Unsere Mission ist es, unseren Gästen nicht nur Unterkunft, sondern auch Inspiration zu bieten.
+          Wir verstehen, dass Reisen nicht nur das Erkunden neuer Orte bedeutet, sondern auch das Eintauchen in eine Kultur, das Erschaffen von Erinnerungen und das Erleben von Luxus in all seinen Facetten.
+        </p>
+        <h3>Unsere Philosophie</h3>
+        <p>
+          Im Boutique-Hotel Technikum Wien sind wir stolz darauf, Tradition und Moderne miteinander zu verbinden.
+          Unser Hotel ist ein Ort, an dem sich Wiener Charme und technischer Fortschritt in harmonischer Eintracht begegnen.
+          Wir glauben fest daran, dass Innovation und Service Hand in Hand gehen können, und unsere Gäste verdienen nur das Beste.
+        </p>
+        <h3>Unser Engagement</h3>
+        <p>
+          Unser Team ist unsere Stärke. Wir sind stolz auf unsere engagierten Mitarbeiter, die Tag für Tag ihr Bestes geben, um sicherzustellen,
+          dass Ihr Aufenthalt in unserem Hotel unvergesslich wird. Wir sind für Sie da, um Ihre Bedürfnisse zu erfüllen, sei es durch Empfehlungen
+          für Aktivitäten in Wien oder durch erstklassigen Service in unserem Restaurant und der Lounge.
+        </p>
+        <h3>Nachhaltigkeit</h3>
+        <p>
+          Als Teil unserer Verantwortung für die Umwelt und die Gesellschaft bemühen wir uns, nachhaltige Praktiken in unser Hotelkonzept zu integrieren.
+          Wir arbeiten kontinuierlich an umweltfreundlichen Initiativen und bemühen uns, unsere ökologische Fußabdruck zu minimieren.
+        </p>
+        <h3>Kontakt</h3>
+        <p>
+          Wir möchten stets mit unseren Gästen in Kontakt bleiben. Ob es sich um Anfragen, Feedback oder besondere Wünsche handelt,
+          wir sind immer erreichbar. Unsere Türen und unser Herz stehen für Sie offen.
+          Vielen Dank, dass Sie sich die Zeit genommen haben, uns näher kennenzulernen.
+          Wir freuen uns darauf, Sie persönlich in unserem Boutique-Hotel Technikum Wien willkommen zu heißen und Ihre Erwartungen zu übertreffen.
+          <br>
+          Ihr Boutique-Hotel Technikum Wien Team
         </p>
       </div>
     </div>
