8b1626772c2bf6a7963fca6a693e760ec7c16cad
Nikolaus.Stift <nstift.work@gmail.com>
2023-11-13 19:34:50 +0100

    Composition API auf Options API geändert

--- src/components/roomList.vue
+++ src/components/roomList.vue
@@ -1,5 +1,5 @@
 <script>
-import { defineComponent, ref, computed, onMounted } from 'vue';
+import { defineComponent } from 'vue';
 import RoomCard from './RoomCard.vue'; // Stelle sicher, dass dieser Pfad korrekt ist
 import { getAllRooms } from '@/api/roomService'; // Stelle sicher, dass dieser Pfad korrekt ist
 import { BButton, BCardGroup } from 'bootstrap-vue-3';
@@ -11,25 +11,41 @@ export default defineComponent({
     BButton,
     BCardGroup,
   },
-  setup() {
-    const rooms = ref([]);
-    const currentPage = ref(1);
-    const limit = 5;
-
-    onMounted(async () => {
-      try {
-        rooms.value = await getAllRooms();
-      } catch (error) {
-        console.error('Failed to fetch rooms:', error);
-      }
+  data() {
+    return {
+      rooms: [],
+      currentPage: 1,
+      limit: 5,
+    };
+  },
+  computed: {
+    totalPages() {
+      return Math.ceil(this.rooms.length / this.limit);
+    },
+    startIndex() {
+      return (this.currentPage - 1) * this.limit;
+    },
+    endIndex() {
+      return this.currentPage * this.limit;
+    },
+    paginatedRooms() {
+      return this.rooms.slice(this.startIndex, this.endIndex);
+    },
+  },
+  methods: {
+    prevPage() {
+      if (this.currentPage > 1) this.currentPage--;
+    },
+    nextPage() {
+      if (this.currentPage < this.totalPages) this.currentPage++;
+    },
+  },
+  mounted() {
+    getAllRooms().then(data => {
+      this.rooms = data;
+    }).catch(error => {
+      console.error('Failed to fetch rooms:', error);
     });
-
-    const totalPages = computed(() => Math.ceil(rooms.value.length / limit));
-    const startIndex = computed(() => (currentPage.value - 1) * limit);
-    const endIndex = computed(() => currentPage.value * limit);
-    const paginatedRooms = computed(() => rooms.value.slice(startIndex.value, endIndex.value));
-
-    return { paginatedRooms, currentPage, totalPages };
   },
 })
 </script>
@@ -39,8 +55,8 @@ export default defineComponent({
     <room-card v-for="room in paginatedRooms" :key="room.id" :room="room" />
   </b-card-group>
   <div class="pagination-buttons">
-    <b-button @click="currentPage--" :disabled="currentPage <= 1">Previous</b-button>
-    <b-button @click="currentPage++" :disabled="currentPage >= totalPages">Next</b-button>
+    <b-button @click="prevPage" :disabled="currentPage <= 1">Previous</b-button>
+    <b-button @click="nextPage" :disabled="currentPage >= totalPages">Next</b-button>
   </div>
 </template>
 
@@ -50,5 +66,4 @@ export default defineComponent({
   justify-content: center;
   margin-top: 20px;
 }
-
 </style>
