a7eb59a08db35f68137acdb03c7c9a44123cccf1
Nikolaus.Stift <nstift.work@gmail.com>
2023-11-13 20:33:39 +0100

    Anpassung header auf bootstrap-vue3. Ich Kotze

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,32 +7,26 @@ export default defineComponent({
 </script>
 
 <template>
-
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container">
-        <div class="d-flex justify-content-between w-100">
-          <router-link to="/" class="navbar-brand">
-            <img
-                alt="Logo"
-                src="@/assets/FH_Technikum_Wien_logo.svg.png"
-                style="max-width: 40%; max-height: 30px; height: auto;"
-            />
-            Technikum
-          </router-link>
-          <div class="d-flex">
-            <!-- Zimmer anzeigen Button -->
-            <router-link to="/rooms" class="btn btn-outline-primary btn-sm me-1">Zimmer <br/>anzeigen</router-link>
-
-            <!-- Anmelden Button -->
-            <button class="btn btn-outline-primary btn-sm me-1">Anmelden</button>
-            <!-- Profil Button -->
-            <button class="btn btn-outline-success btn-sm">Profil</button>
-          </div>
-        </div>
-      </div>
-    </nav>
-
+  <b-navbar toggleable="lg" type="light" variant="light">
+      <b-navbar-brand href="/">
+        <b-img
+            alt="Logo"
+            src="../src/assets/FH_Technikum_Wien_logo.svg.png"
+            height="30"
+        ></b-img>
+        Technikum
+      </b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ms-auto">
+          <b-nav-item href="/rooms">Zimmer anzeigen</b-nav-item>
+          <b-nav-item href="#">Anmelden</b-nav-item>
+          <b-nav-item href="#">Profil</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+  </b-navbar>
 </template>
+
 
 
 
